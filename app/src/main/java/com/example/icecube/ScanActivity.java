cb68e65802edc171f4a83eb63dd37dca10ab5b61
xsluo <xsluo@126.com>
2024-06-03 13:49:32 +0800

    添加了网站跳转

--- app/src/main/java/com/example/icecube/ScanActivity.java
+++ app/src/main/java/com/example/icecube/ScanActivity.java
@@ -19,6 +19,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
